f97a48e83aa196035718d1886147ae5f9b05cd82
Prem0561 <premkumar82713@gmail.com>
2024-02-26 20:19:05 +0530

    new changes

--- test.sh
+++ test.sh
@@ -1,1 +1,14 @@
-echo "Hello, World!"
+#!/bin/bash
+#
+# Author         : Emertxe (https://www.emertxe.com) 
+# Date           : Tue 16 Feb 2016 16:27:07 IST
+# File           : ls_002_ch6_calender.sh
+# Title          : Calender
+# Description    : A simple script to print the calender month by default
+#		     
+
+echo "The Month is"
+cal # cal command displays current month by default
+
+echo "An alternate view of calender"
+ncal # An alternale layout for calender
