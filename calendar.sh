bea01cbc1736f9677c28959ebaf689794b1204f4
Prem0561 <premkumar82713@gmail.com>
2024-02-27 22:19:39 +0530

    new script for calendar

--- calendar.sh
+++ calendar.sh
@@ -1,14 +1,17 @@
 #!/bin/bash
-#
-# Author         : Emertxe (https://www.emertxe.com) 
-# Date           : Tue 16 Feb 2016 16:27:07 IST
-# File           : ls_002_ch6_calender.sh
-# Title          : Calender
-# Description    : A simple script to print the calender month by default
-#		     
-
-echo "The Month is"
-cal # cal command displays current month by default
-
-echo "An alternate view of calender"
-ncal # An alternale layout for calender
+# Shell script to display current date, calendar, and the number of
+# user logged.
+# -------------------------------------------------------------------------
+# Copyright (c) 2005 nixCraft project <http://cyberciti.biz/fb/>
+# This script is licensed under GNU GPL version 2.0 or above
+# -------------------------------------------------------------------------
+# This script is part of nixCraft shell script collection (NSSC)
+# Visit http://bash.cyberciti.biz/ for more information.
+# ----------------------------------------------------------------------
+echo "Today is $(date)"
+echo ""
+ 
+echo "Calendar :"
+cal
+ 
+echo "Number of users currently logged : $(who | wc -l)"
